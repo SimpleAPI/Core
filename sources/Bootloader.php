1f6d2e5342f0075d25808deba215ac6025fff401
Antoine Knockaert <knocka.a@gmail.com>
2014-07-29 11:08:09 +0200

    Removing unused methods

--- sources/Bootloader.php
+++ sources/Bootloader.php
@@ -40,20 +40,6 @@ class Bootloader
         }
     }
 
-    /**
-     * Add headers for access control origin to the response
-     */
-    private static function checkAllowedOrigin()
-    {
-//        if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], Configuration::$config['security.allowedDomains'])) {
-//            self::$response->setHeader('Access-Control-Allow-Headers', $_SERVER['HTTP_ORIGIN']);
-//            self::$response->setHeader('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN']);
-//            self::$response->setHeader('Access-Control-Allow-Credentials', 'true');
-//            self::$response->setHeader('Access-Control-Allow-Headers', $_SERVER['HTTP_ORIGIN']);
-//        }
-//        self::$response->setHeader('Access-Control-Allow-Methods', 'GET,POST');
-    }
-
     public static function boot() {
         self::registerAutoloader();
         self::handlePHPErrors();
